4050f47d0df02395a9cdbe24b580863b890ec09d
Sergey Brykulskii <serxio6758sk@gmail.com>
2023-04-06 11:56:24 +0300

    Добавил классы без описания

--- StaffManager/StaffManager.Domain/Entities/Responsibilities.cs
+++ StaffManager/StaffManager.Domain/Entities/Responsibilities.cs
@@ -1,12 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace StaffManager.Domain.Entities
+﻿namespace StaffManager.Domain.Entities
 {
-    internal class Responsibilities
+    public class Responsibilities
     {
     }
 }
