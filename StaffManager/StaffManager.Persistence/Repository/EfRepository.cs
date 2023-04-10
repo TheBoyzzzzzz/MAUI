56bb70cc3ed9beda2a5d2cfc8d3db84d7dc44972
Sergey Brykulskii <serxio6758sk@gmail.com>
2023-04-10 18:36:10 +0300

    сделал начальную реализацию класса Repository

---
+++ StaffManager/StaffManager.Persistence/Repository/EfRepository.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StaffManager.Domain.Abstractions;
+using StaffManager.Domain.Entities;
+using StaffManager.Persistence.Data;
+
+namespace StaffManager.Persistence.Repository
+{
+    public class EfRepository<T> : IRepository<T> where T : Entity
+    {
+        protected readonly AppDbContext _context;
+        protected readonly DbSet<T> _entities;
+
+        public EfRepository(AppDbContext context)
+        {
+            _context = context;
+            _entities = context.Set<T>();
+        }
+        public Task AddAsync(T entity, CancellationToken cancellationToken = default)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteAsync(T entity, CancellationToken cancellationToken = default)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> FirstOrDefaultAsync(System.Linq.Expressions.Expression<Func<T, bool>> filter, CancellationToken cancellationToken = default)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> GetByIdAsync(int id, CancellationToken cancellationToken = default, params System.Linq.Expressions.Expression<Func<T, object>>[]? includesProperties)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IReadOnlyList<T>> ListAllAsync(CancellationToken cancellationToken = default)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IReadOnlyList<T>> ListAsync(System.Linq.Expressions.Expression<Func<T, bool>> filter, CancellationToken cancellationToken = default, params System.Linq.Expressions.Expression<Func<T, object>>[]? includesProperties)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateAsync(T entity, CancellationToken cancellationToken = default)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
