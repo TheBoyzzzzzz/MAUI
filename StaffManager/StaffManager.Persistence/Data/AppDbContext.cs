8dad853718cfdeaa219713cf8f38124505591b94
Sergey Brykulskii <serxio6758sk@gmail.com>
2023-04-10 17:47:19 +0300

    added appDbContext

---
+++ StaffManager/StaffManager.Persistence/Data/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StaffManager.Domain.Entities;
+
+namespace StaffManager.Persistence.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) 
+            : base(options)
+        {
+            Database.EnsureCreated();
+        }
+        public DbSet<Position> Positions { get; set; }
+        public DbSet<PositionResponsibility> PositionResponsibilities { get; set; }
+    }
+}
