571f4f4ada937552cbe6c6015ff6a30d2eeb9b3b
Sergey Brykulskii <serxio6758sk@gmail.com>
2023-04-13 22:53:57 +0300

    Сделал интерфейсы служебных классов реализующих бизнес-логику

---
+++ StaffManager/StaffManager.Application/Abstractions/IPositionService.cs
@@ -0,0 +1,9 @@
+﻿using StaffManager.Domain.Entities;
+
+namespace StaffManager.Application.Abstractions
+{
+    public interface IPositionService<T> : IBaseService<T> where T : Position
+    {
+        Task<IReadOnlyList<T>> GetResponsibilitiesListAsync(int positionId, CancellationToken cancellationToken = default);
+    }
+}
