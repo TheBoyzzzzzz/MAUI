bc4b71fcdf5756f77fcceef6d235cdccb3b3366a
Timofey Vlasenko <94196049+Tivlas@users.noreply.github.com>
2023-04-16 19:23:42 +0300

    Issue 13. (#14)
    * Добавил список обязанностей в должность.
    
    * Добавил конструкторы Entity классам.
    
    * Изменил имя свойства и удалил лишнее.

--- StaffManager/StaffManager.Domain/Entities/PositionResponsibility.cs
+++ StaffManager/StaffManager.Domain/Entities/PositionResponsibility.cs
@@ -5,12 +5,23 @@ namespace StaffManager.Domain.Entities
     [Table("PositionResponsibilities")]
     public class PositionResponsibility : Entity
     {
+        public PositionResponsibility()
+        {
+        }
+
+        public PositionResponsibility(string name, string description, int importance, int positionId) : base(name)
+        {
+            Description = description;
+            Importance = importance;
+            PositionId = positionId;
+        }
+
         [MaxLength(100)]
         public string Description { get; set; } = string.Empty;
 
         public int Importance { get; init; }
 
         [Indexed]
-        public int PositionID { get; set; }
+        public int PositionId { get; set; }
     }
 }
