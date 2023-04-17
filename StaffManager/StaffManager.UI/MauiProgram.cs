b2bd9bce216a0b07b979667ccce6c7f025591803
Tivlas <tima021003tima@gmail.com>
2023-04-17 18:39:30 +0300

    user-story-8. Регистрация сервисов, папки, установка пакетов.

--- StaffManager/StaffManager.UI/MauiProgram.cs
+++ StaffManager/StaffManager.UI/MauiProgram.cs
@@ -1,24 +1,37 @@
-﻿using Microsoft.Extensions.Logging;
+﻿using CommunityToolkit.Maui;
+using Microsoft.Extensions.Logging;
+using StaffManager.Application.Abstractions;
+using StaffManager.Application.Services;
+using StaffManager.Domain.Abstractions;
+using StaffManager.Persistence.UnitOfWork;
 
 namespace StaffManager.UI;
 
 public static class MauiProgram
 {
-	public static MauiApp CreateMauiApp()
-	{
-		var builder = MauiApp.CreateBuilder();
-		builder
-			.UseMauiApp<App>()
-			.ConfigureFonts(fonts =>
-			{
-				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
-				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
-			});
+    public static MauiApp CreateMauiApp()
+    {
+        var builder = MauiApp.CreateBuilder();
+        builder
+            .UseMauiApp<App>()
+            .UseMauiCommunityToolkit()
+            .ConfigureFonts(fonts =>
+            {
+                fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+            });
 
 #if DEBUG
-		builder.Logging.AddDebug();
+        builder.Logging.AddDebug();
 #endif
+        SetupServices(builder.Services);
+        return builder.Build();
+    }
 
-		return builder.Build();
-	}
+    private static void SetupServices(IServiceCollection services)
+    {
+        services.AddSingleton<IUnitOfWork, FakeUnitOfWork>();
+        services.AddSingleton<IPositionService, PositionService>();
+        services.AddSingleton<IPositionResponsibilityService, PositionResponsibilityService>();
+    }
 }
