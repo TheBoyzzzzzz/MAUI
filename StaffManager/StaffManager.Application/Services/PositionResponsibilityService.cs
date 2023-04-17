6295d8db74951c77cbf52f865a743084aab283cd
Oleg Ershov <97069709+OlegErshov@users.noreply.github.com>
2023-04-17 18:32:16 +0300

    Реализация сервисов слоя Application
    * implementation of FakeUnitOfWork class
    
    * Реализация сервисов для должности и должностных обязанностей
    
    * Исправил ошибки(добавил нужные параметры в методы)
    
    ---------
    
    Co-authored-by: Sergey Brykulskii <serxio6758sk@gmail.com>

---
+++ StaffManager/StaffManager.Application/Services/PositionResponsibilityService.cs
@@ -0,0 +1,56 @@
+﻿using StaffManager.Application.Abstractions;
+using StaffManager.Domain.Abstractions;
+using StaffManager.Domain.Entities;
+using System.Linq.Expressions;
+
+namespace StaffManager.Application.Services
+{
+    public class PositionResponsibilityService : IPositionResponsibilityService
+    {
+        private IUnitOfWork _unitOfWork;
+
+        public PositionResponsibilityService(IUnitOfWork unit)
+        {
+
+            _unitOfWork = unit;
+
+        }
+
+        public Task AddAsync(PositionResponsibility item, CancellationToken cancellationToken = default)
+        {
+            return _unitOfWork.PositionResponsibilityRepository.AddAsync(item, cancellationToken);
+        }
+
+        public Task DeleteAsync(PositionResponsibility item, CancellationToken cancellationToken = default)
+        {
+
+            return _unitOfWork.PositionResponsibilityRepository.DeleteAsync(item, cancellationToken);
+        }
+
+        public Task<PositionResponsibility> FirstOrDefaultAsync(Expression<Func<PositionResponsibility, bool>> filter, CancellationToken cancellationToken = default)
+        {
+            return _unitOfWork.PositionResponsibilityRepository.FirstOrDefaultAsync(filter, cancellationToken);
+        }
+
+        public Task<IReadOnlyList<PositionResponsibility>> GetAllAsync(CancellationToken cancellationToken = default)
+        {
+            return _unitOfWork.PositionResponsibilityRepository.ListAllAsync(cancellationToken);
+        }
+
+        public Task<PositionResponsibility> GetByIdAsync(int id, CancellationToken cancellationToken = default, params Expression<Func<PositionResponsibility, object>>[]? includesProperties)
+        {
+            return _unitOfWork.PositionResponsibilityRepository.GetByIdAsync(id, cancellationToken, includesProperties);
+        }
+
+        public Task<IReadOnlyList<PositionResponsibility>> ListAsync(Expression<Func<PositionResponsibility, bool>> filter, CancellationToken cancellationToken = default, params Expression<Func<PositionResponsibility, object>>[]? includesProperties)
+        {
+            return _unitOfWork.PositionResponsibilityRepository.ListAsync(filter, cancellationToken, includesProperties);
+        }
+
+        public Task UpdateAsync(PositionResponsibility item, CancellationToken cancellationToken = default)
+        {
+            return _unitOfWork.PositionResponsibilityRepository.UpdateAsync(item, cancellationToken);
+        }
+    }
+}
+
---
+++ StaffManager/StaffManager.Application/Services/PositionService.cs
@@ -0,0 +1,64 @@
+﻿using StaffManager.Application.Abstractions;
+using StaffManager.Domain.Abstractions;
+using StaffManager.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaffManager.Application.Services
+{
+    internal class PositionService : IPositionService
+    {
+        private IUnitOfWork _unitOfWork;
+
+        public PositionService(IUnitOfWork unit)
+        {
+            _unitOfWork = unit;
+        }
+
+        public Task AddAsync(Position item, CancellationToken cancellationToken = default)
+        {
+            return  _unitOfWork.PositionRepository.AddAsync(item, cancellationToken);
+        }
+
+        public Task DeleteAsync(Position item, CancellationToken cancellationToken = default)
+        {
+            return  _unitOfWork.PositionRepository.DeleteAsync(item, cancellationToken);
+        }
+
+        public Task<Position> FirstOrDefaultAsync(Expression<Func<Position, bool>> filter, CancellationToken cancellationToken = default)
+        {
+            return _unitOfWork.PositionRepository.FirstOrDefaultAsync(filter, cancellationToken);
+        }
+
+        public Task<IReadOnlyList<Position>> GetAllAsync(CancellationToken cancellationToken = default)
+        {
+            return _unitOfWork.PositionRepository.ListAllAsync(cancellationToken);
+        }
+
+        public Task<Position> GetByIdAsync(int id, CancellationToken cancellationToken = default, params Expression<Func<Position, object>>[]? includesProperties)
+        {
+            return _unitOfWork.PositionRepository.GetByIdAsync(id, cancellationToken, includesProperties);  
+        }
+
+        public Task<IReadOnlyList<PositionResponsibility>> GetResponsibilitiesListAsync(int positionId, CancellationToken cancellationToken = default)
+        {
+            var pos = _unitOfWork.PositionRepository.ListAsync((pos) => pos.Id == positionId, cancellationToken).Result;
+            var res = pos.Select(x => x.PositionResponsibilities).First().ToList().AsReadOnly();
+            return Task.FromResult((IReadOnlyList<PositionResponsibility>)res);
+        }
+
+        public Task<IReadOnlyList<Position>> ListAsync(Expression<Func<Position, bool>> filter, CancellationToken cancellationToken = default, params Expression<Func<Position, object>>[]? includesProperties)
+        {
+            return _unitOfWork.PositionRepository.ListAsync(filter, cancellationToken, includesProperties);
+        }
+
+        public Task UpdateAsync(Position item, CancellationToken cancellationToken = default)
+        {
+            return _unitOfWork.PositionRepository.UpdateAsync(item, cancellationToken);
+        }
+    }
+}
