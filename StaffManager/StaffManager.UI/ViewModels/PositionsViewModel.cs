06ee8a2e48a267cc0c72c967c688f9aaaa868ef0
Sergey Brykulskii <serxio6758sk@gmail.com>
2023-04-17 23:15:35 +0300

    Добавил класс PositionsViewModel

---
+++ StaffManager/StaffManager.UI/ViewModels/PositionsViewModel.cs
@@ -0,0 +1,15 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using StaffManager.Application.Abstractions;
+
+namespace StaffManager.UI.ViewModels;
+
+public partial class PositionsViewModel : ObservableObject
+{
+    private readonly IPositionService _positionService;
+    private readonly IPositionResponsibilityService _positionResponsibilityService;
+    public PositionsViewModel(IPositionService positionService, IPositionResponsibilityService positionResponsibilityService)
+    {
+        _positionService = positionService;
+        _positionResponsibilityService = positionResponsibilityService;
+    }
+}
