0e8b38adf109b25b1b5fa126dda45700f2a13595
Sergey Brykulskii <serxio6758sk@gmail.com>
2023-04-19 22:50:29 +0300

    Дописал класс модели представления для страницы Positions

--- StaffManager/StaffManager.UI/ViewModels/PositionsViewModel.cs
+++ StaffManager/StaffManager.UI/ViewModels/PositionsViewModel.cs
@@ -1,5 +1,8 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 using StaffManager.Application.Abstractions;
+using StaffManager.Domain.Entities;
+using System.Collections.ObjectModel;
 
 namespace StaffManager.UI.ViewModels;
 
@@ -11,5 +14,37 @@ public partial class PositionsViewModel : ObservableObject
     {
         _positionService = positionService;
         _positionResponsibilityService = positionResponsibilityService;
+    }
+    public ObservableCollection<Position> Positions { get; set; } = new();
+    public ObservableCollection<PositionResponsibility> PositionResponsibilities { get; set; } = new();
+
+    [ObservableProperty]
+    private Position selectedPosition;
+
+    [RelayCommand]
+    async Task UpdateGroupList() => await GetPositions();
+    [RelayCommand]
+    async Task UpdateMembersList() => await GetPositionResponsibilities();
+    public async Task GetPositions()
+    {
+        var positions = await _positionService.GetAllAsync();
+        await MainThread.InvokeOnMainThreadAsync(() =>
+        {
+            Positions.Clear();
+            foreach (var position in positions)
+                Positions.Add(position);
+        });
+    }
+    public async Task GetPositionResponsibilities()
+    {
+        var positionResponsibilities = await _positionResponsibilityService
+            .ListAsync(posResp => posResp.PositionId == SelectedPosition.Id);
+
+        await MainThread.InvokeOnMainThreadAsync(() =>
+        {
+            PositionResponsibilities.Clear();
+            foreach (var positionResponsibility in positionResponsibilities)
+                PositionResponsibilities.Add(positionResponsibility);
+        });
     }
 }
