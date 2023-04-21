b3cd873e66b6540a2e3731304539c7d1e552b057
olezh <olezhka1629@gmail.com>
2023-04-21 17:38:36 +0300

    неправильная версия picker

--- StaffManager/StaffManager.UI/Pages/Positions.xaml.cs
+++ StaffManager/StaffManager.UI/Pages/Positions.xaml.cs
@@ -1,9 +1,15 @@
+using StaffManager.UI.ViewModels;
+
 namespace StaffManager.UI.Pages;
+
 
 public partial class Positions : ContentPage
 {
-	public Positions()
+	private PositionsViewModel _positionsModel;
+	public Positions(PositionsViewModel posMod)
 	{
 		InitializeComponent();
+		_positionsModel = posMod;
+		BindingContext = _positionsModel;
 	}
 }
