bf5167e778e841e0d43ee0f8ca0c49fcdade1864
Tivlas <tima021003tima@gmail.com>
2023-04-25 00:32:30 +0300

    Добавил конвертер важности в цвет.

---
+++ StaffManager/StaffManager.UI/ValueConverters/ImportanceToColorValueConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaffManager.UI.ValueConverters;
+public class ImportanceToColorValueConverter : IValueConverter
+{
+    private Color Important = Colors.LightPink;
+    private Color Default = Colors.LightGreen;
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if ((int)value > 2)
+        {
+            return Important;
+        }
+        return Default;
+    }
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) => throw new NotImplementedException();
+}
