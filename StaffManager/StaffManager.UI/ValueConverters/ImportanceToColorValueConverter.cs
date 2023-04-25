aaca0ce442672b0a58b972decf2ad028f3ce12f0
Timofey Vlasenko <94196049+Tivlas@users.noreply.github.com>
2023-04-25 11:28:35 +0300

    Выделение важных обязанностей цветом (#23)
    * implementation of FakeUnitOfWork class
    
    * неправильная версия picker
    
    * Зарегистрировал страницу как сервис, теперь она появляется
    
    * Добавил  collectionView для обязанностей работников, исправил метод ListAsync в FakePositionResponsibilityRepository
    
    * Переименовал некоторые объекты (в помощь Олегу).
    
    * Слегка почистил код.
    
    * Поправил отображение текста.
    
    * Поправил текст при отображении обязанностей.
    
    * Добавил конвертер важности в цвет.
    
    * Добавил отображение важности и выделение цветом в зависимости от ее значения.
    
    * Code CleanUp.
    
    * Сделал поля цвета readonly.
    
    ---------
    
    Co-authored-by: olezh <olezhka1629@gmail.com>
    Co-authored-by: Sergey Brykulskii <serxio6758sk@gmail.com>

---
+++ StaffManager/StaffManager.UI/ValueConverters/ImportanceToColorValueConverter.cs
@@ -0,0 +1,17 @@
+﻿using System.Globalization;
+
+namespace StaffManager.UI.ValueConverters;
+public class ImportanceToColorValueConverter : IValueConverter
+{
+    private readonly Color Important = Colors.LightPink;
+    private readonly Color Default = Colors.LightGreen;
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if ((int)value > 2)
+        {
+            return Important;
+        }
+        return Default;
+    }
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) => throw new NotImplementedException();
+}
