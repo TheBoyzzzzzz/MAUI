410c93dd83549cf91a3887d2037cf98e698231fe
Tivlas <tima021003tima@gmail.com>
2023-04-25 15:45:34 +0300

    Регистрация сервиса БД и заполнение БД полурандомными данными.

--- StaffManager/StaffManager.UI/MauiProgram.cs
+++ StaffManager/StaffManager.UI/MauiProgram.cs
@@ -1,10 +1,13 @@
 ﻿using System.Reflection;
 using CommunityToolkit.Maui;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 using StaffManager.Application.Abstractions;
 using StaffManager.Application.Services;
 using StaffManager.Domain.Abstractions;
+using StaffManager.Domain.Entities;
+using StaffManager.Persistence.Data;
 using StaffManager.Persistence.UnitOfWork;
 using StaffManager.UI.ViewModels;
 
@@ -37,12 +40,15 @@ public static class MauiProgram
         SetupServices(builder.Services);
         SetupViewModels(builder.Services);
         SetupViews(builder.Services);
+
+        AddDbContext(builder);
+        SeedData(builder.Services);
         return builder.Build();
     }
 
     private static void SetupServices(IServiceCollection services)
     {
-        services.AddSingleton<IUnitOfWork, FakeUnitOfWork>();
+        services.AddSingleton<IUnitOfWork, EfUnitOfWork>();
         services.AddSingleton<IPositionService, PositionService>();
         services.AddSingleton<IPositionResponsibilityService, PositionResponsibilityService>();
     }
@@ -51,8 +57,60 @@ public static class MauiProgram
     {
         services.AddSingleton<PositionsViewModel>();
     }
+
     private static void SetupViews(IServiceCollection services)
     {
         services.AddTransient<Pages.Positions>();
+    }
+
+    private static void AddDbContext(MauiAppBuilder builder)
+    {
+        var connStr = builder.Configuration
+         .GetConnectionString("SqliteConnection");
+        string dataDirectory = string.Empty;
+#if ANDROID
+        dataDirectory = FileSystem.AppDataDirectory + "/";
+#endif
+        connStr = string.Format(connStr, dataDirectory);
+        var options = new DbContextOptionsBuilder<AppDbContext>()
+       .UseSqlite(connStr)
+       .Options;
+        builder.Services.AddSingleton((s) =>
+         new AppDbContext(options));
+    }
+
+    public async static Task SeedData(IServiceCollection services)
+    {
+        using var provider = services.BuildServiceProvider();
+        var unitOfWork = provider.GetRequiredService<IUnitOfWork>();
+        await unitOfWork.RemoveDatbaseAsync();
+        await unitOfWork.CreateDatabaseAsync();
+
+        var positions = new List<Position>()
+        {
+            new Position {Name = "SEO", Salary=1000, Id = 1},
+            new Position {Name = "Manager", Salary = 500, Id = 2},
+            new Position { Name = "Employee", Salary = 100, Id = 3}
+        };
+
+        foreach (var pos in positions)
+        {
+            await unitOfWork.PositionRepository.AddAsync(pos);
+        }
+        await unitOfWork.SaveAllAsync();
+
+        Random rnd = new();
+        for (int i = 0; i < 15; i++)
+        {
+            await unitOfWork.PositionResponsibilityRepository.AddAsync(new PositionResponsibility()
+            {
+                Name = $"Responsibility#{i + 1}",
+                Description = $"Description#{i + 1}",
+                Id = i + 1,
+                Importance = rnd.Next(1, 7),
+                Position = positions[rnd.Next(1, 10) % positions.Count()]
+            });
+        }
+        await unitOfWork.SaveAllAsync();
     }
 }
