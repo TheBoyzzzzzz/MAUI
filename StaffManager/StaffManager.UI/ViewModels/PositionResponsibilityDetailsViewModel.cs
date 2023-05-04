111ee732c14ea263d2a35933bad8b44c4cfbd8a6
Tivlas <tima021003tima@gmail.com>
2023-05-04 23:07:53 +0300

    Изменил логику в PositionResponsibilityDetailsViewModel.

--- StaffManager/StaffManager.UI/ViewModels/PositionResponsibilityDetailsViewModel.cs
+++ StaffManager/StaffManager.UI/ViewModels/PositionResponsibilityDetailsViewModel.cs
@@ -1,31 +1,17 @@
-﻿using StaffManager.Domain.Entities;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using StaffManager.Domain.Entities;
 using System.ComponentModel;
 using System.Runtime.CompilerServices;
 
 namespace StaffManager.UI.ViewModels;
 
-public class PositionResponsibilityDetailsViewModel : IQueryAttributable,  INotifyPropertyChanged
+public partial class PositionResponsibilityDetailsViewModel : ObservableObject, IQueryAttributable
 {
-    public event PropertyChangedEventHandler PropertyChanged;
-
-    PositionResponsibility selectedObject;
-
-    public PositionResponsibility SelectedObject
-    {
-        get => selectedObject;
-        set
-        {
-            selectedObject = value;
-            OnPropertyChanged();
-        }
-    }
+   [ObservableProperty] public PositionResponsibility _selectedObject;
 
     public void ApplyQueryAttributes(IDictionary<string, object> query)
     {
         SelectedObject = query["PositionResponsibility"] as PositionResponsibility;
     }
-
-    public void OnPropertyChanged([CallerMemberName] string name = "") =>
-        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
 }
 
