949446f7399e93fbe94520e23aa48c26afb65668
Tivlas <tima021003tima@gmail.com>
2023-05-06 17:01:35 +0300

    Добавил ViewModel для AddPositionPage.

--- StaffManager/StaffManager.UI/Pages/AddPositionPage.xaml.cs
+++ StaffManager/StaffManager.UI/Pages/AddPositionPage.xaml.cs
@@ -1,9 +1,15 @@
+using StaffManager.UI.ViewModels;
+
 namespace StaffManager.UI.Pages;
 
 public partial class AddPositionPage : ContentPage
 {
-	public AddPositionPage()
+    private readonly AddPositionViewModel _vm;
+
+    public AddPositionPage(AddPositionViewModel vm)
 	{
 		InitializeComponent();
-	}
+        _vm = vm;
+        BindingContext = _vm;
+    }
 }
