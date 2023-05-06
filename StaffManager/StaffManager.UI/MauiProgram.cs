62a5658ac25182f78a80afe9621e4e37172c6686
Sergey Brykulskii <serxio6758sk@gmail.com>
2023-05-06 22:08:46 +0300

    Удалил метод заполнения бд

--- StaffManager/StaffManager.UI/MauiProgram.cs
+++ StaffManager/StaffManager.UI/MauiProgram.cs
@@ -6,7 +6,6 @@ using Microsoft.Extensions.Logging;
 using StaffManager.Application.Abstractions;
 using StaffManager.Application.Services;
 using StaffManager.Domain.Abstractions;
-using StaffManager.Domain.Entities;
 using StaffManager.Persistence.Data;
 using StaffManager.Persistence.UnitOfWork;
 using StaffManager.UI.Pages;
@@ -42,7 +41,6 @@ public static class MauiProgram
         SetupViewModels(builder.Services);
         SetupViews(builder.Services);
         AddDbContext(builder);
-        SeedData(builder.Services);
         return builder.Build();
     }
 
@@ -76,44 +74,11 @@ public static class MauiProgram
         string dataDirectory = string.Empty;
 #if ANDROID
         dataDirectory = FileSystem.AppDataDirectory + "/";
+#else
+        dataDirectory = AppDomain.CurrentDomain.BaseDirectory + Path.DirectorySeparatorChar;
 #endif
         connStr = string.Format(connStr, dataDirectory);
         var options = new DbContextOptionsBuilder<AppDbContext>().UseSqlite(connStr).Options;
         builder.Services.AddSingleton((s) => new AppDbContext(options));
-    }
-
-    public async static Task SeedData(IServiceCollection services)
-    {
-        using var provider = services.BuildServiceProvider();
-        var unitOfWork = provider.GetRequiredService<IUnitOfWork>();
-        await unitOfWork.RemoveDatbaseAsync();
-        await unitOfWork.CreateDatabaseAsync();
-
-        var positions = new List<Position>()
-        {
-            new Position {Name = "SEO", Salary=1000, Id = 1},
-            new Position {Name = "Manager", Salary = 500, Id = 2},
-            new Position { Name = "Employee", Salary = 100, Id = 3}
-        };
-
-        foreach (var pos in positions)
-        {
-            await unitOfWork.PositionRepository.AddAsync(pos);
-        }
-        await unitOfWork.SaveAllAsync();
-
-        Random rnd = new();
-        for (int i = 0; i < 15; i++)
-        {
-            await unitOfWork.PositionResponsibilityRepository.AddAsync(new PositionResponsibility()
-            {
-                Name = $"Responsibility#{i + 1}",
-                Description = $"Description#{i + 1}",
-                Id = i + 1,
-                Importance = rnd.Next(1, 7),
-                Position = positions[rnd.Next(1, 10) % positions.Count()]
-            });
-        }
-        await unitOfWork.SaveAllAsync();
     }
 }
