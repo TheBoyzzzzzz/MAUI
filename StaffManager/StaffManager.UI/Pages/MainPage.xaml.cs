419a5d466bf6e3d5ae7c9300aa62fda472dba210
Tivlas <tima021003tima@gmail.com>
2023-05-06 16:57:25 +0300

    Добавил кнопки на главной странице. Описал для нее ViewModel. Создал две страницы.

---
+++ StaffManager/StaffManager.UI/Pages/MainPage.xaml.cs
@@ -0,0 +1,16 @@
+﻿using StaffManager.UI.ViewModels;
+
+namespace StaffManager.UI.Pages;
+
+public partial class MainPage : ContentPage
+{
+    private readonly MainViewModel _vm;
+
+    public MainPage(MainViewModel vm)
+    {
+        InitializeComponent();
+        _vm = vm;
+        BindingContext = _vm;
+    }
+}
+
