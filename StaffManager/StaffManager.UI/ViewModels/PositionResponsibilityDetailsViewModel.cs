45acabdfdc7994a3b505169677eb8e25f11ef798
Sergey Brykulskii <serxio6758sk@gmail.com>
2023-05-08 21:00:00 +0300

    Сделал редактирвание и перемещение объекта в другую группу

--- StaffManager/StaffManager.UI/ViewModels/PositionResponsibilityDetailsViewModel.cs
+++ StaffManager/StaffManager.UI/ViewModels/PositionResponsibilityDetailsViewModel.cs
@@ -1,4 +1,6 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using StaffManager.Application.Abstractions;
 using StaffManager.Domain.Entities;
 
 namespace StaffManager.UI.ViewModels;
@@ -6,10 +8,31 @@ namespace StaffManager.UI.ViewModels;
 public partial class PositionResponsibilityDetailsViewModel : ObservableObject, IQueryAttributable
 {
     [ObservableProperty] public PositionResponsibility _selectedObject;
+    [ObservableProperty] private Position _selectedPosition;
+
+    private readonly IPositionResponsibilityService _positionResponsibilityService;
+    public PositionStorage PositionStorage { get; }
+
+    [RelayCommand] async Task UpdatePositionResponsibility() => await Update();
+    public PositionResponsibilityDetailsViewModel(IPositionResponsibilityService positionResponsibilityService,
+        PositionStorage positionStorage)
+    {
+        _positionResponsibilityService = positionResponsibilityService;
+        PositionStorage = positionStorage;
+    }
 
     public void ApplyQueryAttributes(IDictionary<string, object> query)
     {
         SelectedObject = query["PositionResponsibility"] as PositionResponsibility;
+        SelectedPosition = SelectedObject.Position;
+
+    }
+
+    private async Task Update()
+    {
+        SelectedObject.Position = SelectedPosition;
+        await _positionResponsibilityService.UpdateAsync(SelectedObject);
+        await _positionResponsibilityService.SaveChangesAsync();
     }
 }
 
